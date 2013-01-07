0a75e4c754f2bc18cdfa04bd428bea0585d3a205
Doru Arfire <doruarfire@gmail.com>
2013-01-07 10:33:14 +0200

    Refactor in progress

--- screp/actions.py
+++ screp/actions.py
@@ -5,7 +5,97 @@ from lxml.cssselect import CSSSelector
 
 from .context import XMLContext
 
-element_type = lxml.etree._Element
+
+supported_types = ['element', 'element_set', 'string', 'context']
+
+
+def types_match(t1, t2):
+    return (t1 is None) or (t2 is None) or (t1 == t2)
+
+
+class BasePipeUnit(object):
+    in_type = None
+    out_type = None
+
+
+    def can_follow(self, other):
+        return types_match(other.out_type, self.in_type)
+
+
+    def can_precede(self, other):
+        return types_match(self.out_type, other.in_type)
+
+
+    def execute(self, value, *extras):
+        pass
+
+
+class GenericPipeUnit(BasePipeUnit):
+    def __init__(self, f, in_type=None, out_type=None, name=None, extras=None):
+        self.name = name
+        self.f = f
+        self.in_type = in_type
+        self.out_type = out_type
+        if extras is None:
+            extras = []
+        self.extras = extras
+
+
+    def execute(self, value):
+        return self.f(value, *self.extras)
+
+
+    def __str__(self):
+        return "PipeAction<%s(%s) => %s>" % (self.name, self.in_type, self.out_type)
+
+
+class SelectorPipeUnit(GenericPipeUnit):
+    def __init__(self, f, in_type=None, out_type=None, name=None, extras=None):
+        super(SelectorPipeUnit, self).__init__(f, in_type=in_type, out_type=out_type, name=name, extras=extras)
+
+        self.selector = CSSSelector(self.extras[0])
+        del self.extras[0]
+
+
+    def execute(self, value):
+        return self.f(value, self.selector, *self.extras)
+
+
+def make_unit_of_class(cls, f, in_type, out_type):
+    def f(name, extras):
+        return cls(f, in_type=in_type, out_type=out_type, name=name, extras=extras)
+
+    return f
+
+
+def make_generic_unit(f, in_type, out_type):
+    return make_unit_of_class(GenericPipeUnit, f, in_type, out_type)
+
+
+def make_selector_unit(f, in_type, out_type):
+    return make_unit_of_class(SelectorPipeUnit, f, in_type, out_type)
+
+
+actions = [
+        # accessors
+        (('first', 'f'),    make_generic_unit(lambda s: s[0], 'element_set', 'element')),
+        (('last', 'l'),     make_generic_unit(lambda s: s[-1], 'element_set', 'element')),
+        (('class', 'c'),    make_generic_unit(lambda e: e.get('class'), 'element', 'string')),
+        (('parent', 'p'),   make_generic_unit(lambda e: e.getparent(), 'element', 'element')),
+        (('text', 't'),     make_generic_unit(lambda e: e.text, 'element', 'string')),
+        (('tag',),          make_generic_unit(lambda e: e.tag, 'element', 'string')),
+        (('attr', 'a'),     make_generic_unit(lambda e, a: e.get(a), 'element', 'string')),
+        (('nth', 'n'),      make_generic_unit(lambda s, i: s[i], 'element_set', 'element')),
+        (('desc', 'd'),     make_selector_unit(lambda e, sel: sel(e), 'element', 'element_set')),
+        (('first-desc', 'fd'),  make_selector_unit(lambda e, sel: sel(e)[0], 'element', 'element')),
+
+        # functions/filters
+        (('upper',)         make_generic_unit(lambda s: s.upper(), 'string', 'string')),
+        (('lower',)         make_generic_unit(lambda s: s.lower(), 'string', 'string')),
+        (('trim',)         make_generic_unit(lambda s: s.strip(), 'string', 'string')),
+        ]
+
+
 
 class TermAction(object):
     def __init__(self, f, validator=None, f_args=None, name=None):
