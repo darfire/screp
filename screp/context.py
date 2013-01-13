f6cf52c4e240e4eb8581bf9607c12f09f43a8099
Doru Arfire <doru@rndsoft.ro>
2013-01-13 22:43:22 +0200

    Execution context support for secondary anchors.

--- screp/context.py
+++ screp/context.py
@@ -5,3 +5,13 @@ class XMLContext(object):
     def __init__(self, current, root):
         self.current = current
         self.root = root
+
+        self._anchors = {'$': current, '@': root}
+
+
+    def get_anchor(self, name):
+        return self._anchors[name]
+
+
+    def add_anchor(self, name, value):
+        self._anchors[name] = value
